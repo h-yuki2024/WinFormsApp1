c0998541e688028dd478e32fd562cd061578b5cb
h-yuki <h-yuki@maple-soft.co.jp>
2024-03-12 09:16:04 +0900

    ログイン画面とメニュー画面

--- WinFormsApp1/Menu.cs
+++ WinFormsApp1/Menu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
-    public partial class LoginOK : Form
+    public partial class Menu : Form
     {
-        public LoginOK()
+        public Menu()
         {
             InitializeComponent();
         }
